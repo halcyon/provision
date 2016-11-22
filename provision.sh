bb40cf21cc5f8a8668a3637ac45eca9f99876d4b
Scott McLeod <halcyonblue@gmail.com>
2016-11-22 00:44:30 -0500

    Add kiwix-setup

--- provision.sh
+++ provision.sh
@@ -2,6 +2,32 @@
 
 user=smcleod
 
+install_packages() {
+    typeset -U shell
+    shell=("autojump" "tmux" "stow")
+
+    typeset -U utilities
+    utilities=("pass" "the_silver_searcher" "openconnect" "unzip" "emacs-nox"
+               "git" "jdk8-openjdk" "rclone" "sbcl")
+
+    typeset -U bluetooth
+    utilities=("bluez" "bluez-utils")
+
+    typeset -U xorg
+    xorg=("xf86-input-synaptics" "xorg-server" "xorg-server-utils" "xorg-apps"
+          "xorg-xinit" "xclip" "konsole" "ttf-symbola" "ttf-ubuntu-font-family"
+          "noto-fonts" "flashplugin" "firefox" "calibre")
+
+    typeset -U aur
+    aur=("aur-git" "powerpill" "leiningen-standalone" "tmate" "totp-cli" "dropbox"
+         "dropbox-cli" "slack-desktop" "kiwix-bin" "ttf-fira-code")
+
+    aura --noconfirm --needed -S ${shell}
+    aura --noconfirm --needed -S ${utilities}
+    aura --noconfirm --needed -S ${xorg}
+    aura --noconfirm --needed -A ${aur}
+}
+
 clone_repos() {
 sudo -iu ${user} zsh <<EOF
   if [[ ! -d ~/dotfiles ]]
@@ -73,33 +99,24 @@ EOF
 fi
 }
 
-install_packages() {
-    typeset -U shell
-    shell=("autojump" "tmux" "stow")
+setup_kiwix() {
+  if [[ ! -f /etc/systemd/system/multi-user.target.wants/kiwix-serve.service ]]
+  then
+    cp ${PWD}/kiwix-serve.service /etc/systemd/system/kiwix-serve.service
+    chown -R root:root /etc/systemd/system/kiwix-serve.service
+    systemctl enable kiwix-serve.service
+  fi
+}
 
-    typeset -U utilities
-    utilities=("pass" "the_silver_searcher" "openconnect" "unzip" "emacs-nox"
-               "git" "jdk8-openjdk" "rclone" "sbcl")
-
-    typeset -U bluetooth
-    utilities=("bluez" "bluez-utils")
-
-    typeset -U xorg
-    xorg=("xf86-input-synaptics" "xorg-server" "xorg-server-utils" "xorg-apps"
-          "xorg-xinit" "xclip" "konsole" "ttf-symbola" "ttf-ubuntu-font-family"
-          "noto-fonts" "flashplugin" "firefox" "calibre")
-
-    typeset -U aur
-    aur=("aur-git" "powerpill" "leiningen-standalone" "tmate" "totp-cli" "dropbox"
-         "dropbox-cli" "slack-desktop" "kiwix-bin" "ttf-fira-code")
-
-    aura --noconfirm --needed -S ${shell}
-    aura --noconfirm --needed -S ${utilities}
-    aura --noconfirm --needed -S ${xorg}
-    aura --noconfirm --needed -A ${aur}
+setup_dropbox() {
+  if [[ ! -f /etc/systemd/system/multi-user.target.wants/dropbox@smcleod.service ]]
+  then
+    sudo systemctl enable dropbox@smcleod
+  fi
 }
 
 install_packages
 clone_repos
 install_quicklisp
 install_stumpwm
+setup_kiwix
